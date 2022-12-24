e7f98bef9fa71df91e175795f318875c6641e576
StrixROX <40167899+StrixROX@users.noreply.github.com>
2022-12-24 11:51:06 +0530

    Changed data passing syntax in FormElements

--- components/FormElements/FormElements.jsx
+++ components/FormElements/FormElements.jsx
@@ -1,13 +1,16 @@
 import InputElement from './InputElement'
 import ElementGroup from './ElementGroup'
+import RadioElement from './RadioElement'
 
 export default function generateElement(key, schema) {
 	const type = schema.uiType
 
 	if (type === 'Input') {
-		return <InputElement key={key} {...schema} />
+		return <InputElement key={key} schema={schema} />
 	} else if (type === 'Group') {
-		return <ElementGroup key={key} {...schema} />
+		return <ElementGroup key={key} schema={schema} />
+	} else if (type === 'Radio') {
+		return <RadioElement key={key} schema={schema} />
 	} else {
 		return (
 			<p>
