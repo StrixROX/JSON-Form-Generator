9bd9599e840c6a906f95eb4f8441d2875842fa17
StrixROX <40167899+StrixROX@users.noreply.github.com>
2022-12-26 03:42:13 +0530

    Changed color and tootltip margin in Description component

--- components/FormElements/Description.jsx
+++ components/FormElements/Description.jsx
@@ -4,8 +4,8 @@ export default function Description({ description }) {
 	return (
 		<>
 			<div className="description-wrapper relative inline overflow-y-visible">
-				<span className="icon peer ml-2.5 cursor-help">ℹ️</span>
-				<div className="description pointer-events-none absolute inline-block w-max max-w-xs rounded-md border border-purple-200 bg-purple-100 px-3 py-2 text-purple-400 opacity-0 transition-all peer-hover:pointer-events-auto peer-hover:opacity-100">
+				<span className="icon peer ml-2.5  cursor-help">ℹ️</span>
+				<div className="description pointer-events-none absolute ml-3 inline-block w-max max-w-xs rounded-md border border-purple-200 bg-purple-100 px-3 py-2 font-light text-black opacity-0 transition-all peer-hover:pointer-events-auto peer-hover:opacity-100">
 					{description.trim()}
 				</div>
 			</div>
