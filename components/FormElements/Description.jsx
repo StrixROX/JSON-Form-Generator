3241c7a2294c7d1d356d42dd7644a151fedb11b1
StrixROX <40167899+StrixROX@users.noreply.github.com>
2022-12-26 02:01:19 +0530

    Specified text color in Description element tootip

--- components/FormElements/Description.jsx
+++ components/FormElements/Description.jsx
@@ -5,7 +5,7 @@ export default function Description({ description }) {
 		<>
 			<div className="description-wrapper relative inline overflow-y-visible">
 				<span className="icon peer mx-3 cursor-help">ℹ️</span>
-				<div className="description pointer-events-none absolute inline-block w-max max-w-xs rounded-md border border-purple-200 bg-purple-100 px-3 py-2 opacity-0 transition-all peer-hover:pointer-events-auto peer-hover:opacity-100">
+				<div className="description pointer-events-none absolute inline-block w-max max-w-xs rounded-md border border-purple-200 bg-purple-100 px-3 py-2 text-purple-400 opacity-0 transition-all peer-hover:pointer-events-auto peer-hover:opacity-100">
 					{description.trim()}
 				</div>
 			</div>
