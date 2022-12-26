df002f40870fe286d46cff1e2d3bf5ba35ab062f
StrixROX <40167899+StrixROX@users.noreply.github.com>
2022-12-26 16:12:21 +0530

    Changed font color in Description tooltip

--- components/FormElements/Description.jsx
+++ components/FormElements/Description.jsx
@@ -5,7 +5,7 @@ export default function Description({ description }) {
 		<>
 			<div className="description-wrapper relative inline overflow-y-visible">
 				<img src="/info.svg" alt="" className="peer ml-2.5 inline h-4 w-4" />
-				<div className="description pointer-events-none absolute ml-3 inline-block w-max max-w-xs rounded-md border border-purple-200 bg-purple-100 px-3 py-2 font-light text-black opacity-0 transition-all peer-hover:pointer-events-auto peer-hover:opacity-100">
+				<div className="description pointer-events-none absolute ml-3 inline-block w-max max-w-xs rounded-md border border-purple-200 bg-purple-100 px-3 py-2 text-neutral-500 opacity-0 transition-all peer-hover:pointer-events-auto peer-hover:opacity-100">
 					{description.trim()}
 				</div>
 			</div>
